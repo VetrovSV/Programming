f75f4e1aaacfa0a5b8b0e2dd7dbce9329c3c9487
VetrovSV <eridani_alpha@hotmail.com>
2025-10-14 22:24:33 +0900

    + examples, upd: plan

--- examples/gradio/show_plot/main.py
+++ examples/gradio/show_plot/main.py
@@ -4,18 +4,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def plot_seaborn_scatter(data):
-    plt.figure(figsize=(6,4))
-    seaborn.scatterplot(data=data, x="x_column", y="y_column")
-    fig = plt.gcf()  # получить текущую фигуру
+    fig = plt.figure(figsize=(6,4))
+    seaborn.scatterplot(data=data, x="x", y="y")
+    # fig = plt.gcf()  # получить текущую фигуру
     return fig
 
-df = pd.DataFrame({
-    "x_column": [1, 2, 3, 4],
-    "y_column": [10, 15, 13, 17]
-})
 
 gr.Interface(
     fn=plot_seaborn_scatter, 
-    inputs=gr.Dataframe(headers=["x_column", "y_column"]), 
+    inputs=gr.Dataframe(headers=["x", "y"]), 
     outputs=gr.Plot()
 ).launch()
